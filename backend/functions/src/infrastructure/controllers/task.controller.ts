70d52a4a8611347cd63434e7d7faa2ef6c205cdb
nicolas roa <nroa2024@gmail.com>
2025-04-26 11:24:24 +1000

    Se repara y refactoriza la obtencion del userId

--- backend/functions/src/infrastructure/controllers/task.controller.ts
+++ backend/functions/src/infrastructure/controllers/task.controller.ts
@@ -2,18 +2,20 @@ import { Request, Response } from 'express';
 import {FirestoreTaskRepository} from "../../infrastructure/repositories/task.repository";
 import {TaskService} from "../../application/services/task.service";
 import {CreateTaskDto} from "../../domain/dtos/create-task.dto";
+import {getUserIdFromRequest} from "../../shared/jwt";
 
 const taskRepo = new FirestoreTaskRepository();
 const taskService = new TaskService(taskRepo);
 
 export const createTaskHandler = async (request: Request, response: Response) => {
     try{
-        const { userId, title, description, completed } = request.body as CreateTaskDto;
+        const userId = getUserIdFromRequest(request);
+        const { title, description, completed } = request.body as CreateTaskDto;
 
         if(!userId || !title || !description) {
+            console.log(request.body as CreateTaskDto)
             return response.status(400).json({ message: 'Todo los campos son requeridos.' });
         }
-
         const task = await taskService.createTask({ userId, title, description, completed });
 
         return response.status(201).json(task);
--- backend/functions/src/shared/jwt.ts
+++ backend/functions/src/shared/jwt.ts
@@ -37,3 +37,21 @@ export const generateRefreshToken = (user: User): string => {
 
     return jwt.sign(payload, secret, { expiresIn: '7d' });
 }
+
+export const getUserIdFromRequest = (request: any): string => {
+    let authHeader: string | null | undefined = undefined;
+    if (typeof (request.headers as any).get === 'function') {
+        authHeader = (request.headers as any).get('authorization');
+    } else {
+        authHeader = (request.headers as any)['authorization'] || (request.headers as any)['Authorization'];
+    }
+
+    if (!authHeader) {
+        throw new Error('No token provided');
+    }
+
+    const token = authHeader.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'Atom') as any;
+
+    return decoded.sub;
+};
